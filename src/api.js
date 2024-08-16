a56a757defc06dddd4a0432ec012ddb8bd9b4e1d
muzikamin <muzikamin@naver.com>
2024-08-16 14:57:10 +0900

    오류3

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 const fetch = require("node-fetch");
 
 const baseUrl =
-  "http://apis.data.go.kr/6260000/AttractionService/getAttractionKr";
+  "https://apis.data.go.kr/6260000/AttractionService/getAttractionKr";
 
-const foodBaseUrl = "http://apis.data.go.kr/6260000/FoodService/getFoodKr";
+const foodBaseUrl = "https://apis.data.go.kr/6260000/FoodService/getFoodKr";
 
 const themeBaseUrl =
-  "http://apis.data.go.kr/6260000/RecommendedService/getRecommendedKr";
+  "https://apis.data.go.kr/6260000/RecommendedService/getRecommendedKr";
 
 const serviceKey =
   "G47LjQsbye2mab%2BmzvLhCkT6QWR4fSIKUIJe1sRLo8Y0J28Jo5ES5f%2Bv8KPOISJjO1VIBQhEmDVIef0WSnS1IQ%3D%3D";
